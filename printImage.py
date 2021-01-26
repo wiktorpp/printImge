4c84346ed636669e1dfd5b8a5eb4cf3f66a66571
wiktorpp <grazynapp65@gmail.com>
2021-01-26 19:15:55 +0100

    Fixed #1

--- printImage.py
+++ printImage.py
@@ -7,12 +7,13 @@ from heart import heart as bitmap
 size = [len(bitmap), len(bitmap[0])]
 for row in range(size[0]):
     if row%2 != 0:
-        print(f"{esc}[0m")
         continue
-    for col in range(size[1]):
-        pixelUpper =bitmap[row][col]
-        try: pixelLower = bitmap[row + 1][col]
-        except: pixelLower = [0, 0, 0]
-        sys.stdout.write(f"{esc}[38;2;{pixelUpper[0]};{pixelUpper[1]};{pixelUpper[2]}m")
-        sys.stdout.write(f"{esc}[48;2;{pixelLower[0]};{pixelLower[1]};{pixelLower[2]}m")
-        sys.stdout.write("▀")
+    else:
+        for col in range(size[1]):
+            pixelUpper =bitmap[row][col]
+            try: pixelLower = bitmap[row + 1][col]
+            except: pixelLower = [0, 0, 0]
+            sys.stdout.write(f"{esc}[38;2;{pixelUpper[0]};{pixelUpper[1]};{pixelUpper[2]}m")
+            sys.stdout.write(f"{esc}[48;2;{pixelLower[0]};{pixelLower[1]};{pixelLower[2]}m")
+            sys.stdout.write("▀")
+        print(f"{esc}[0m")
