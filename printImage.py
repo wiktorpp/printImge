4ffd2cc11916f37675215c10c706f3e3a2c66ada
wiktorpp <grazynapp65@gmail.com>
2021-01-27 15:23:53 +0100

    Changed the colour of the last line from black to translucent (default).

--- printImage.py
+++ printImage.py
@@ -12,9 +12,11 @@ for row in range(size[0]):
     else:
         for col in range(size[1]):
             pixelUpper =bitmap[row][col]
-            try: pixelLower = bitmap[row + 1][col]
-            except: pixelLower = [0, 0, 0]
             sys.stdout.write(f"{esc}[38;2;{pixelUpper[0]};{pixelUpper[1]};{pixelUpper[2]}m")
-            sys.stdout.write(f"{esc}[48;2;{pixelLower[0]};{pixelLower[1]};{pixelLower[2]}m")
+            try:
+                pixelLower = bitmap[row + 1][col]
+                sys.stdout.write(f"{esc}[48;2;{pixelLower[0]};{pixelLower[1]};{pixelLower[2]}m")
+            except:
+                sys.stdout.write(f"{esc}[49m")
             sys.stdout.write("▀")
         print(f"{esc}[0m")
