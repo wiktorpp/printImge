ad6daa9d4d0126536c6a64acb3d5565c29bba174
wiktorpp <grazynapp65@gmail.com>
2021-01-27 15:31:17 +0100

    Cleanup

--- printImage.py
+++ printImage.py
@@ -12,10 +12,14 @@ for row in range(size[0]):
     else:
         for col in range(size[1]):
             pixelUpper =bitmap[row][col]
-            sys.stdout.write(f"{esc}[38;2;{pixelUpper[0]};{pixelUpper[1]};{pixelUpper[2]}m")
+            sys.stdout.write(
+                f"{esc}[38;2;{pixelUpper[0]};{pixelUpper[1]};{pixelUpper[2]}m"
+            )
             try:
                 pixelLower = bitmap[row + 1][col]
-                sys.stdout.write(f"{esc}[48;2;{pixelLower[0]};{pixelLower[1]};{pixelLower[2]}m")
+                sys.stdout.write(
+                    f"{esc}[48;2;{pixelLower[0]};{pixelLower[1]};{pixelLower[2]}m"
+                )
             except:
                 sys.stdout.write(f"{esc}[49m")
             sys.stdout.write("▀")
